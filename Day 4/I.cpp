fe8d19e259e6615c2b07f7505f5ec1032d717b66
Nanmanas Poonyapat <38101663+NnmnsP@users.noreply.github.com>
2022-12-09 09:17:22 +0700

    add I.cpp

--- Day 4/I.cpp
+++ Day 4/I.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main() {
-    int a, b, common;
+    int a, b, temp, common;
     cin >> a >> b;
-    while (a > 0 && b > 0) {
+    while (true) {
         if (a == b) {
             common = a;
             break;
         }
-
-        if (a%2 == 0) {
-            a /= 2;
-        } else {
-            a = (a-1)/2;
+        
+        if (a > b) {
+            temp = a;
+            a = b;
+            b = temp;
         }
-
-        if (b%2 == 0) {
-            b /= 2;
-        } else {
+        
+        if (b%2 == 1) {
             b = (b-1)/2;
+        } else {
+            b /= 2;
         }
+        
     }
     cout << common;
 }
